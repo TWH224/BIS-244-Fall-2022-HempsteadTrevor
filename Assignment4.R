17adedc15e4b6f4362922d8d1b4a5a35cde7564f
Rishika <rig220@lehigh.edu>
2022-11-21 10:51:18 -0500

    Grading

--- Assignment4.R
+++ Assignment4.R
@@ -5,6 +5,12 @@ library(ggplot2)
 
 DS1 <- read.csv("WOT.csv")
 
+#Failed to filter for battles more than 3 : -10
+#Failed to convert to numeric datatype : -10
+
+DS1$WR <- as.numeric(sub("%", "",DS1$WR,fixed=TRUE))
+DS1 <- DS1 %>% filter(Battles>=4)
+
 G <- ggplot(data = DS1 , mapping = aes(x = Nation, y = WR, group=Nation)) 
 
 G + geom_boxplot()
